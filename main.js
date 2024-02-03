62e5300785e7b0e3886b0bf635af15f468b7f7f7
JessCP25 <jpayanoc25@gmail.com>
2024-02-02 19:31:39 -0500

    Agregando validaciones

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-const resultado = document.querySelector('#result');
+const resultado = document.querySelector("#result");
+const inputVacio = document.querySelector('#result--vacio');
+const inputMensaje = document.querySelector('#result--mensaje');
 
 const encriptar = () => {
   const input = document.querySelector("#input");
+
+  // QUITAR ESPACIOS
+
+  const valorInput = input.value.trim();
+
+  if(valorInput === '') return;
+
+  inputMensaje.classList.remove('inactive');
+  inputVacio.classList.add('inactive');
+
+  // VERIFICA SI SOLO SON MINÚSCULAS - CARACTERES - ACENTOS
+  if (!verificar(input.value)) return;
+  
   let valor = input.value.split("");
+
   const nuevoMensaje = valor.map((letra) => {
     switch (letra) {
       case "a":
@@ -19,13 +35,23 @@ const encriptar = () => {
         return letra;
     }
   });
-  console.log(nuevoMensaje.join(""));
+  input.value = "";
   resultado.value = nuevoMensaje.join("");
 };
 
 const desencriptar = () => {
   const input = document.querySelector("#input");
-  let valor = input.value;
+  
+  // QUITAR ESPACIOS
+  let valor = input.value.trim();
+
+  if(valor === '') return;
+
+  inputMensaje.classList.remove('inactive');
+  inputVacio.classList.add('inactive');
+
+  // VERIFICA SI SOLO SON MINÚSCULAS - CARACTERES - ACENTOS
+  if (!verificar(valor)) return;
 
   if (valor.includes("ai")) {
     valor = valor.replace("ai", "a");
@@ -43,7 +69,19 @@ const desencriptar = () => {
     valor = valor.replace("ufat", "u");
   }
 
-  console.log(valor);
+  input.value = "";
 
   resultado.value = valor;
+};
+
+const copiar = () => {
+  resultado.select();
+
+  document.execCommand("copy");
+  resultado.setSelectionRange(0,0);
+};
+
+const verificar = (input) => {
+  const verificador = /^[a-z]+$/;
+  return verificador.test(input) ? true : false;
 };
